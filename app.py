a40231029c969bcb9f547f71a106e5623b46c050
FizaAamir <144325138+FizaAamir@users.noreply.github.com>
2025-05-09 15:03:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from src.utils import generate_creative_brief
 from src.models import UserInput
 from src.constants import user_input_data
 
+
 def main():
 
     user_input = UserInput(**user_input_data)
     result = generate_creative_brief(user_input)
     print(json.dumps(result, indent=2))
+
 
 if __name__ == "__main__":
     main()
